e0dd675b6f2c178fef89675f2ab99964bd2ad897
gabriel <gabriel.santos.m112@gmail.com>
2021-01-17 00:17:27 -0300

    Sugestions in HomePage

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AnDesigns from "react-native-vector-icons/AntDesign";
+import {Header, Wrapper, BalanceContainer, BalanceTitle, Balance, Container} from './styles';
+
+import Sugestions from '../../components/Sugestions';
 
 const Home = () => {
   return (
-    <View >
-      <Text>Home</Text>
-    </View>
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <AnDesigns name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Sugestions />
+      </Container>
+      
+    </Wrapper>
   );
-}
-
-
+};
 
 export default Home;
